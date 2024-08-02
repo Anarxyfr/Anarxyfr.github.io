47db01fd74e22e4d51c82e3a7c573b2091a10b4b
Anarxy <95134169+Anarxyfr@users.noreply.github.com>
2024-08-02 14:43:49 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,25 @@
+document.querySelectorAll('.server').forEach(server => {
+    server.addEventListener('click', () => {
+        const genre = server.querySelector('.server-icon span').innerText;
+        let content = `<h2>${genre} Games</h2><p>List of ${genre} games will be displayed here.</p>`;
+
+        if (genre === 'Horror') {
+            content = `<h2>${genre} Games</h2>
+                        <ul>
+                            <li><a href="https://wellsousaaa.github.io/Five-Nights-at-Freddys-Web/" target="_blank" onclick="maskLink(this)">Five Nights at Freddy's</a></li>
+                            <!-- Add more horror games links here -->
+                        </ul>`;
+        }
+
+        document.querySelector('.genre-content').innerHTML = content;
+        document.querySelector('.header').style.display = 'none';
+        document.querySelector('.welcome-content').style.display = 'none';
+        document.querySelector('.genre-content').style.display = 'block';
+    });
+});
+
+function maskLink(element) {
+    setTimeout(() => {
+        element.href = 'about:blank';
+    }, 1000);
+}
