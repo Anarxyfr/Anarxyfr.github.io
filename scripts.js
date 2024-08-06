8d869c9d7dd5e0f966bde2ef8a54551b1e14d146
Anarxy <95134169+Anarxyfr@users.noreply.github.com>
2024-08-06 13:44:12 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,10 +4,9 @@ document.querySelectorAll('.server').forEach(server => {
         let content = `<h2>${genre} Games</h2><p>List of ${genre} games will be displayed here.</p>`;
 
         if (genre === 'Horror') {
-            const url = `https://wellsousaaa.github.io/Five-Nights-at-Freddys-Web/`;
             content = `<h2>${genre} Games</h2>
                         <ul>
-                            <li><a href="${url}" target="_blank">Five Nights at Freddy's</a></li>
+                            <li><a href="https://wellsousaaa.github.io/Five-Nights-at-Freddys-Web/" target="_blank" onclick="maskLink(this)">Five Nights at Freddy's</a></li>
                             <!-- Add more horror games links here -->
                         </ul>`;
         }
@@ -18,3 +17,5 @@ document.querySelectorAll('.server').forEach(server => {
         document.querySelector('.genre-content').style.display = 'block';
     });
 });
+
+}
